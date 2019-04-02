924e41a765e70664a046d56ebd7cb9774359a0f2
Bryan D. Tumba <bryan.delama@utec.edu.pe>
2019-04-02 15:44:05 -0500

    Algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,11 +9,9 @@ int main() {
 	int high, low;
 	int swap;
 
-	while (cin >> high >> low){
+	while ((scanf("%d %d", &high, &low))==2){
 		if ( low > high){
-			swap = high;
-			high = low;
-			low = swap;
+			std::swap(high, low);
 		}
 		int maxCycle = 0;
 		int low_print = low;
@@ -21,12 +20,7 @@ int main() {
 			unsigned int n = low;
 			while (n != 1){
 				count++;
-				if (n % 2 == 0){
-					n = n/2;
-				}
-				else {
-					n = 3*n + 1;
-				}
+				n = (n % 2 == 0)? n = n /2 : n = 3 * n+1;
 			}
 			if (count >= maxCycle){
 				maxCycle = count;
@@ -34,5 +28,6 @@ int main() {
 		low++;
 	}
 	printf("%d %d %d\n", low_print, high, maxCycle);
-	}	
+	}
+	return 0;
 }
