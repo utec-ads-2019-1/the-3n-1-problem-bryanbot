4619a5c611cf4045110f9072231ad1cbca96b3e7
Bryan D. Tumba <bryan.delama@utec.edu.pe>
2019-04-02 16:25:27 -0500

    Con fe

--- main.cpp
+++ main.cpp
@@ -14,14 +14,12 @@ int main() {
 		int maxCycle = 0;
 		for (int i = low; i <= high; i++){
 			int count = 1;
-			int n = i;
+			unsigned int n = i;
 			while (n != 1){
 				count++;
 				n = (n % 2 == 0)? n / 2 : 3 * n + 1;
 			}
-			if (count >= maxCycle){
-				maxCycle = count;
-			}
+			maxCycle = (count >= maxCycle)? count:maxCycle;
 	}
 	printf("%d %d %d\n", low, high, maxCycle);
 	}
